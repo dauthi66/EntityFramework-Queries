eeedadde42f478f671fa183ad4925b052b8c6b20
Kris <dauthi66@gmail.com>
2022-01-25 17:51:20 -0800

    reverse engineered AP database

---
+++ EntityFramework-Queries/ContactUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFramework_Queries
+{
+    public partial class ContactUpdate
+    {
+        public int VendorId { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+    }
+}
